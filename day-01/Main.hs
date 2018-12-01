a8f58be471d7a075fa4b28f42dfcb2ed754f8004
Paolo <mrtnpaolo@gmail.com>
2018-12-01 10:33:17 +0100

    🎄 day-01 part 2

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -3,6 +3,9 @@
 
 module Main where
 
+import Data.Set (Set)
+import qualified Data.Set as Set
+
 parse :: String -> Int
 parse ('+':xs) = read xs
 parse xs       = read xs
@@ -11,4 +14,14 @@ main :: IO ()
 main = do
   input <- map parse . lines <$> readFile "input.txt"
   print $ sum input
+  print $ firstDup $ cycle input
+
+firstDup :: [Int] -> Maybe Int
+firstDup = firstJust . map fst . scanl find ( Nothing , Set.empty ) . scanl (+) 0
+  where
+    find ( Nothing , seen ) f | f `Set.member` seen = ( Just f, Set.insert f seen )
+    find (     ans , seen ) f                       = ( ans   , Set.insert f seen )
+
+    firstJust (Just x : _ ) = Just x
+    firstJust (     _ : xs) = firstJust xs
 
