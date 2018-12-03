35452e9e03c71580be71e50595183d0a2d610d7f
Paolo <mrtnpaolo@gmail.com>
2018-12-03 17:52:27 +0100

    🛠 day-01 in Haskell part 1 cleanup

--- day-03/Main.hs
+++ day-03/Main.hs
@@ -9,8 +9,8 @@ main :: IO ()
 main = do
   input <- readFile "input.txt"
   let byID = parse input
-  let fabric = collect byID
-  print fabric
+  let fabric = draw byID
+  print . Map.size . Map.filter (>1) $ fabric
 
 -- String -> Map ID ( (x,y) , (w,h) )
 parse = toMap . map words . lines . map clean
@@ -23,20 +23,16 @@ parse = toMap . map words . lines . map clean
         fromRect [i,x,y,w,h] = (i,((x,y),(w,h)))
 
 -- Map ID ((x,y),(w,h)) -> Map (x,y) ???
--- collect :: Map Int ((Int,Int),(Int,Int)) -> (Int,Int)
-collect m = Map.size . Map.filter (>1) . draw $ m
+draw m = draw' m
   where
-    (maxX,maxY) = foldl collect' (0,0) m
-    collect' (mx,my) ((x,y),(w,h)) = (max mx xw,max my yh)
-      where
-        xw=x+w
-        yh=y+h
+    (maxX,maxY) = foldl collect (0,0) m
+    collect (mx,my) ((x,y),(w,h)) = (max mx (x+w),max my (y+h))
 
     coords = [(x,y) | x <- [0..maxX-1], y <- [0..maxY-1]]
 
     rect (x,y) (w,h) = [(x_,y_) | x_ <- [x..x+w-1], y_ <- [y..y+h-1]]
 
-    draw = Map.foldl' drawOne Map.empty
+    draw' = Map.foldl' drawOne Map.empty
     drawOne fabric = foldl' (flip $ Map.alter drawCoord) fabric . uncurry rect
     drawCoord Nothing = Just 1
     drawCoord (Just n) = Just (n+1)
