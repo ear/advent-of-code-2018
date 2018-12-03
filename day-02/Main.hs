0195f002e6d18987dfb08864ea359c59e8a1edca
Paolo <mrtnpaolo@gmail.com>
2018-12-03 16:38:33 +0100

    🎄 day-02 in Haskell part 2
    (from yesterday, forgot to commit)

--- day-02/Main.hs
+++ day-02/Main.hs
@@ -7,6 +7,10 @@ main = do
   -- input <- lines <$> readFile "test.txt"
   input <- lines <$> readFile "input.txt"
   print . checksum . map count $ input
+  let ((xs,ys):_) = search input
+  putStrLn xs
+  putStrLn ys
+  putStrLn $ common xs ys
 
 count = tt . map length . group . sort
 
@@ -14,4 +18,16 @@ count = tt . map length . group . sort
 tt xs = (length $ filter (2==) xs,length $ filter (3==) xs)
 
 checksum xs = length (filter ((>0) . fst) xs) * length (filter ((>0) . snd) xs)
+
+-- search for the two correct box IDs
+search xs = concatMap diffs $ map (\ys -> zip (repeat ys) xs) xs
+
+diffs = concatMap (uncurry diff)
+
+diff xs ys =
+  case length $ filter not $ zipWith (==) xs ys of
+    1 -> [(xs,ys)]
+    _ -> []
+
+common xs ys = fst $ unzip $ filter (uncurry (==)) $ zip xs ys
 
