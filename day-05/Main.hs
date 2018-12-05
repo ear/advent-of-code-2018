75e797b7793f31c3333f9576aa6e8c8ebcbb9b23
Paolo <mrtnpaolo@gmail.com>
2018-12-05 13:46:58 +0100

    🛠 day-05 cleanup

--- day-05/Main.hs
+++ day-05/Main.hs
@@ -7,7 +7,6 @@ import Data.Char
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.Function
 
 main = do
   input <- head . T.lines <$> T.readFile "input.txt"
@@ -15,35 +14,34 @@ main = do
 
 units cs
   = case T.uncons cs of
-      Nothing -> []
-      Just (c,cs) | T.length cs == 0 -> [T.singleton c]
-      Just _  -> r : units rs
+      Nothing                 -> []
+      Just (c,cs) | T.null cs -> [T.singleton c]
+      Just _                  -> r : units rs
         where
-          (r,rs) = takeWhileAlternating cs
+          (r,rs) = unit cs
 
-takeWhileAlternating :: Text -> (Text,Text)
-takeWhileAlternating text
-  = ans
+-- returns (prefix matching and alternating, rest)
+unit :: Text -> (Text,Text)
+unit text
+  | T.null text || T.null (T.tail text) = (T.empty,T.empty)
+  | otherwise                           = T.splitAt (n+1) text
     where
       Just (c,cs) = T.uncons text
-      n = length . takeWhile matching . zip (repeat $ toLower c) $ T.zip text $ T.tail text
-      matching (c,(a,b)) = c == toLower a && c == toLower b && alternating a b
-      ans = T.splitAt (n+1) text
+      n = length . takeWhile (matching $ toLower c) . T.zip text $ cs
 
-alternating c c'
-  | toLower c == toLower c'
-    = (isUpper c && isLower c') || (isLower c && isUpper c')
-  | otherwise = False
+matching c (a,b) = c == toLower a && c == toLower b && alternating a b
+
+alternating a b = (isUpper a && isLower b) || (isLower a && isUpper b)
 
 react cs
   = case T.uncons cs of
       Nothing -> error "empty unit"
-      Just (c,cs) | T.null cs -> T.singleton c
+      Just (c,cs) | T.null cs          -> T.singleton c
                   | even $ T.length cs -> T.singleton c
-                  | otherwise -> T.empty
+                  | otherwise          -> T.empty
 
 step = T.concat . map react . units
 
-fixedPoint input = fst $ head $ dropWhile (uncurry (/=)) $ zip ans (tail ans)
+fixedPoint input = fst . head . dropWhile (uncurry (/=)) . zip ans . tail $ ans
   where
     ans = iterate step input
