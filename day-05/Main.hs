58773f743227b272ddc36eb6f5100e73d9c39fea
Paolo <mrtnpaolo@gmail.com>
2018-12-05 12:04:31 +0100

    🛠 day-05 wrong answer 10508 too low

---
+++ day-05/Main.hs
@@ -0,0 +1,37 @@
+-- cCc c
+-- cCcC ""
+-- cCcCc cCc c
+
+import Data.Ord
+import Data.Char
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Function
+
+main = do
+  input <- head . T.lines <$> T.readFile "input.txt"
+  print $ T.length input
+  -- print $ units input
+  -- print $ step input
+  print $ T.length $ fixedPoint input
+
+units = T.groupBy ((==) `on` toLower)
+
+react cs
+  = case T.uncons cs of
+      Nothing -> error "empty unit"
+      Just (c,cs) | T.null cs -> T.singleton c
+                  | even $ T.length cs -> T.singleton c
+                  | otherwise -> T.empty
+
+step = T.concat . map react . units
+
+fixedPoint input = fst $ head $ dropWhile (uncurry (/=)) $ zip ans (tail ans)
+  where
+    ans = iterate step input
+
+-- react cs@(_:[]) = cs
+-- react cs@(_:_:[]) = []
+-- react (_:_:cs) = react cs
+
