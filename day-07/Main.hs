0ce221648bd3d401a39a10113647ea5d63f24d2e
Paolo <mrtnpaolo@gmail.com>
2018-12-07 11:52:44 +0100

    🛠 day-07 part 2: removed the jobs too early, darn
    ```
    *Main> mapM_ print . construct $ g
    0	'C' 3	.	.	.	.	.	[('A',""),('B',"A"),('D',"A"),('E',"BDF"),('F',"")]
    1	'C' 2	'A' 1	'F' 6	.	.	.	[('B',""),('D',""),('E',"BD")]
    2	'C' 1	.	'F' 5	'B' 2	'D' 4	.	[('E',"")]
    3	.	'E' 5	'F' 4	'B' 1	'D' 3	.	[]
    4	.	'E' 4	'F' 3	.	'D' 2	.	[]
    5	.	'E' 3	'F' 2	.	'D' 1	.	[]
    6	.	'E' 2	'F' 1	.	.	.	[]
    7	.	'E' 1	.	.	.	.	[]
    8	.	.	.	.	.	.	[]
    ```

--- day-07/Main.hs
+++ day-07/Main.hs
@@ -1,8 +1,13 @@
+{-# language RecordWildCards #-}
+{-# language PatternSynonyms #-}
 module Main where
 
 import Data.Ord
+import Data.Map  ( Map )
 import Data.Char
+import Data.Maybe
 
+import qualified Data.Map  as M
 import qualified Data.List as L
 
 main :: IO ()
@@ -14,6 +19,9 @@ main = do
   let g = mkGraph cs input
   -- solution
   putStrLn $ crawl g
+  -- part 2
+  print $ start g
+  print . tick $ start g
 
 -- Parsing
 
@@ -39,29 +47,126 @@ compareNodes = comparing (length . reqs_) <> comparing name_
 
 -- Graph
 
-type Graph = [Node]
+newtype Graph = Graph [Node]
+  deriving (Eq, Ord)
 
-mkGraph cs input = map (\c -> Node c $ map snd . filter ((c==) . fst) $ input) cs
+mkGraph cs input = Graph $ map (\c -> Node c $ map snd . filter ((c==) . fst) $ input) cs
 
 (=-) :: Graph -> Node -> Graph
-g =- n = concatMap (remove n) g
+(Graph g) =- n = Graph $ concatMap (remove n) g
 
 remove (Node c (_:_)) _ = error "removing connected node"
 remove (Node c _) (Node c' _) | c == c' = []
 remove (Node c _) (Node c' rs) = [Node c' (rs L.\\ [c])]
 
 pop :: Graph -> (Node,Graph)
-pop [] = error "empty graph"
+pop (Graph []) = error "empty graph"
 
-pop g@[n@(Node c [])] = (n,[])
-pop [Node c _] = error "edges without nodes"
+pop g@(Graph [n@(Node c [])]) = (n,Graph [])
+pop (Graph [Node c _]) = error "edges without nodes"
 
-pop g = (m,g =- m)
+pop g@(Graph ns) = (m,g =- m)
   where
-    m = minimum g
+    m = minimum ns
 
-crawl [] = []
+crawl (Graph []) = []
 crawl g = c : crawl g'
   where
     (n,g') = pop g
     c = name_ n
+
+empty :: Graph -> Bool
+empty (Graph []) = True
+empty _ = False
+
+freeNodes :: Graph -> Maybe ([Node],Graph)
+freeNodes (Graph ns) | null ns = Nothing
+freeNodes (Graph ns)
+  = case L.partition (null . reqs_) ns of
+      ([],_  ) -> Nothing
+      (fs,ns') -> Just (fs, g')
+        where
+          g' = L.foldl' (=-) (Graph ns') fs
+
+-- Parallel workers
+
+data Worker = Worker
+  { id_ :: Int
+  , job_ :: Maybe (Char, Int) -- job char and job time left
+  }
+
+pattern Idle <- Worker _ Nothing
+pattern Working c t <- Worker _ (Just (c,t))
+
+data Work = Work
+  { t_  :: Int
+  , ws_ :: Map Int Worker
+  , g_  :: Graph
+  }
+
+start :: Graph -> Work
+start = Work (-1) (M.fromList $ zip [1..] $ map (\i -> Worker i Nothing) [1..6])
+
+workerDone = isNothing . job_
+
+workersDone :: Work -> Bool
+workersDone Work{..} = all workerDone ws_
+
+tick :: Work -> Maybe Work
+tick w@Work{..} | workersDone w && empty g_ = Nothing
+tick w@Work{..}
+  = case freeWorkers w of
+      [] -> Just w'
+      ws -> case availableSteps w of
+              Nothing -> Just w'
+              Just (cs,g') -> Just $ stepWorkers (assignWork w (zip ws cs) g')
+  where
+    w' = stepWorkers w
+
+freeWorkers :: Work -> [Int]
+freeWorkers Work{..} = M.keys . M.filter (isNothing . job_) $ ws_
+
+availableSteps :: Work -> Maybe ([Char],Graph)
+availableSteps Work{..}
+  = (\(ns,g) -> (map name_ ns, g)) <$> freeNodes g_
+
+-- only called if there are both freeWorkers and availableStep
+assignWork :: Work -> [(Int,Char)] -> Graph -> Work
+assignWork Work{..} pairs g' = Work t_ ws' g'
+  where
+    ws' = L.foldl' assign ws_ pairs
+    -- m = workers map; i = worker id; c = job char
+    assign m (i,c) = M.insert i (Worker i (Just (c,ord c - ord 'A' + 2))) m
+
+stepWorkers :: Work -> Work
+stepWorkers Work{..} = w'
+  where
+    w' = Work t' ws' g_
+    t' = succ t_
+    ws' = fmap advance ws_
+
+    advance w@Idle = w
+    advance w@(Working c t)
+      = case pred t of
+          0 -> w { job_ = Nothing }
+          n -> w { job_ = Just (c,n) }
+
+construct :: Graph -> [Work]
+construct = L.unfoldr (fmap (\x->(x,x)) . tick) . start
+
+-- Pretty-printing
+
+instance Show Worker where
+  showsPrec _ Worker{..} = showJob job_
+    where
+      showJob Nothing = showString "."
+      showJob (Just (c,t)) = shows c . showChar ' ' . shows t
+
+instance Show Work where
+  showsPrec _ Work{..}
+    = shows t_ . showChar '\t'
+    . showString (L.intercalate "\t" (map show $ M.elems ws_)) . showChar '\t'
+    . shows g_
+
+instance Show Graph where
+  showsPrec _ (Graph ns) = shows . map (\(Node c rs) -> (c,rs)) $ ns
