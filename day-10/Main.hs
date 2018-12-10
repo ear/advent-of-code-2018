b771aadc11e6bda9e818361a1e49686f74c00aad
Paolo <mrtnpaolo@gmail.com>
2018-12-10 09:58:18 +0100

    🛠 day-10 implemented the Sky… wrong!
    I got what I wanted, a map of positions to velocities.
    Unfortunately Map does not give me a mapKeys that changes the keys...
    In this code I end up keeping the positions fixed and modifying the vs.
    Dang.

--- day-10/Main.hs
+++ day-10/Main.hs
@@ -1,8 +1,16 @@
+{-# language ViewPatterns #-}
+
 module Main where
 
+import Data.Map.Strict ( Map )
+
+import qualified Data.Map.Strict as M
+import qualified Data.List       as L
 
 type P = (Int,Int) -- Position
 type V = (Int,Int) -- Velocity
+
+-- Parse
 
 type Entry = (P,V) -- one input entry
 
@@ -10,7 +18,39 @@ parse :: String -> Entry
 parse = (\[a,b,c,d] -> ((a,b),(c,d))) . map read . words . map clean
   where clean c | c `elem` "0123456789-" = c | otherwise = ' '
 
-main = do
-  input <- map parse . lines <$> readFile "test.txt"
-  mapM_ print input
+-- Sky
 
+type Sky = Map P V
+
+mkSky :: [Entry] -> Sky
+mkSky = M.fromList
+
+showLight :: Sky -> P -> Char
+showLight s c | c `M.member` s = '#' | otherwise = '.'
+
+showSky :: Sky -> String
+showSky s = L.intercalate "\n" [ [ showLight s (x,y) | x <- rangeX ] | y <- rangeY ]
+  where
+    (minMax -> (xm,xM), minMax -> (ym,yM)) = unzip . M.keys $ s
+    rangeX = [xm .. xM]
+    rangeY = [ym .. yM]
+
+minMax :: (Ord a, Foldable f) => f a -> (a,a)
+minMax xs = (minimum xs, maximum xs)
+
+p :: Sky -> IO ()
+p = putStrLn . showSky
+
+-- Time
+
+tick :: Sky -> Sky
+tick = M.mapWithKey move
+
+move :: P -> V -> P
+move (x,y) (vx,vy) = (x+vx,x+vy)
+
+-- Main
+
+main = do
+  s <- mkSky . map parse . lines <$> readFile "test.txt"
+  mapM_ (\s -> p s >> putChar '\n') . iterate tick $ s
