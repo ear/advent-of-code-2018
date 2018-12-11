5365507fae4bba75c20278c627f3d8669ec7f762
Paolo <mrtnpaolo@gmail.com>
2018-12-11 12:04:48 +0100

    🎄 day-11 part 2 in Haskell

--- day-11/Main.hs
+++ day-11/Main.hs
@@ -3,14 +3,16 @@ module Main where
 import Data.Set ( Set )
 import qualified Data.Set as S
 
-import Data.Map.Strict ( Map, (!?) )
+import Data.Map.Strict ( Map, (!) )
 import qualified Data.Map.Strict as M
 
 import Data.Ord
 import Data.Foldable
+import qualified Data.List as L
 
 input = 7139
 -- input = 18
+-- input = 42
 
 -- (1,1) ... (300,1)
 -- ...          ...
@@ -53,13 +55,36 @@ coords = [ (x,y) | x <- [1..300], y <- [1..300] ]
 
 --
 
--- sizes = [1..300]
--- 
+sizes = [1..300]
+
 type Size = Int
--- 
--- powers :: Map (Size,Coord) Power
 
+powers :: Map (Size,Coord) Power
+powers = L.foldl' go size1 (tail sizes)
+  where
+    size1 = M.mapKeys (\c -> (1,c)) grid
+    -- compute all the powers for a given size
+    -- by breaking in half and using the previous half-size squares
+    go m s
+      | even s = -- use 4 even-sized ones
+          let s2 = floor (fromIntegral s / 2)
+              -- subsquares
+              ss = [ ( (x,y)
+                     , sum $ map (m !) $ map (\c->(s2,c)) [ (x,y), (x+s2,y), (x,y+s2), (x+s2,y+s2) ] )
+                   | (x,y) <- inside ]
+          in (M.fromList $ map (\(c,p) -> ((s,c),p)) ss) `M.union` m
+      | otherwise = -- use an even one, and many 1-sized ones
+          let se = s-1 -- size of the even one
+              ss = [ ( c , (m ! (se,c)) + sum (map (m !) (extra c)) ) | c <- inside ]
+              extra (x,y) = map (\c -> (1,c)) $ concat
+                [ [ (x+s-1,y') | y' <- [y..y+s-2] ] -- right edge
+                , [ (x',y+s-1) | x' <- [x..x+s-2] ] -- bottom edge
+                , [ (x+s-1,y+s-1) ] ]
+          in (M.fromList $ map (\(c,p) -> ((s,c),p)) ss) `M.union` m
+      where
+        inside = filter (not . outside s) coords
 
+--
 
 search :: Size -> Grid -> (Size, Grid)
 search size g = maximumBy (comparing fst) $ ss
@@ -70,4 +95,7 @@ search size g = maximumBy (comparing fst) $ ss
 
 main = do
   print . topleft . snd . search 3 $ grid
-  print . fmap topleft . maximumBy (comparing fst) $ [ search n grid | n <- [1..300] ]
+  --print . fmap topleft . maximumBy (comparing fst) $ [ search n grid | n <- [1..300] ]
+  let maxpower = maximum powers
+  print maxpower
+  print . L.find ((maxpower ==) . snd) . M.toList $ powers
