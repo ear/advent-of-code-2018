236531fdbb7b34614f7f87c422c5eab21852fa51
Paolo <mrtnpaolo@gmail.com>
2018-12-14 22:07:09 +0100

    🎄 day-14 part 1 in Haskell (STUArrays are fast!)

--- day-14/Main.hs
+++ day-14/Main.hs
@@ -1,5 +1,4 @@
-{-# language TypeApplications #-}
-{-# language RecordWildCards #-}
+{-# language FlexibleContexts #-}
 
 module Main where
 
@@ -9,32 +8,37 @@ import Data.Int
 import Data.Word
 
 import Control.Monad.ST
+import Control.Monad.ST.Unsafe
 import qualified Data.Array.ST as ST
 import qualified Data.Array.MArray as M
 import qualified Data.Array.Unboxed as U
 import qualified Data.Array.IArray as I
 
-type T = U.UArray Word32 Int8
+-- https://www.vex.net/~trebla/haskell/annote-STArray.xhtml
+-- type STUA s = (Word32,Word32) -> Int8 -> ST s (ST.STUArray s Word32 Int8)
+type STUA s = (Int,Int) -> Int-> ST s (ST.STUArray s Int Int)
 
-part1 :: Int -> [Int]
-part1 = takeWhile (>=0) . map fromIntegral . I.elems . make
-
-make :: Int -> T
-make n = ST.runSTUArray $
-  do arr <- ST.newArray (0,fromIntegral n+12) (-1)
-     size <- begin arr
-     _ <- tick arr size n
-     return arr
-
-begin a = do
-  M.writeArray a 0 3
-  M.writeArray a 1 7
-  return 2
-
-tick a size n = go n size 0 1
+--part1 :: Int -> [Int]
+--part1 = takeWhile (>=0) . map fromIntegral . I.elems . make
+part1 n = search predicate extract (n+10)
   where
-    go 0 size _  _  = return size
-    go n size e1 e2 = do
+    predicate l _ = l > n+10
+    extract _ = concat . map show . take 10 . drop n
+
+type Predicate = Int -> [Int] -> Bool
+type Extract a = Int -> [Int] -> a
+
+search :: Predicate -- ^ predicate
+       -> Extract a -- ^ compute answer
+       -> Int       -- ^ maximum iterations
+       -> Maybe a   -- ^ answer
+search p e n = runST $
+  do a <- (ST.newArray :: STUA s) (0,fromIntegral n + 12) (-1)
+     s <- begin a
+     go n a s 0 1
+  where
+    go 0 a _    _  _  = return Nothing
+    go n a size e1 e2 = do
       d1 <- M.readArray a e1
       d2 <- M.readArray a e2
       let (x,y) = (d1+d2) `divMod` 10
@@ -42,15 +46,21 @@ tick a size n = go n size 0 1
                 | otherwise = size + 1
       let e1' = (e1 + fromIntegral d1 + 1) `mod` size'
           e2' = (e2 + fromIntegral d2 + 1) `mod` size'
-      dump <- takeWhile (>=0) <$> M.getElems a
       if x > 0
-      then do M.writeArray a size x
+      then do M.writeArray a size     x
               M.writeArray a (size+1) y
-              traceShow dump $ go (pred n) size' e1' e2'
-      else do M.writeArray a size y
-              traceShow dump $ go (pred n) size' e1' e2'
+      else do M.writeArray a size     y
+      xs <- unsafeInterleaveST (takeWhile (>=0) <$> M.getElems a)
+      if p (fromIntegral size') (map fromIntegral xs)
+      then return $ Just $ e (fromIntegral size') (map fromIntegral xs)
+      else go (pred n) a size' e1' e2'
+
+begin a = do
+  M.writeArray a 0 3
+  M.writeArray a 1 7
+  return 2
 
 input = 846601
 
 main = do
-  print $ part1 16 -- print the example
+  print $ part1 input
