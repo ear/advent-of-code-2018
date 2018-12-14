89429531629a92ce46a6016875e190f91f404976
Paolo <mrtnpaolo@gmail.com>
2018-12-14 19:26:28 +0100

    🛠 day-14 mutable array version

---
+++ day-14/Main.hs
@@ -0,0 +1,54 @@
+{-# language TypeApplications #-}
+{-# language RecordWildCards #-}
+
+module Main where
+
+import Debug.Trace
+
+import Data.Int
+import Data.Word
+
+import Control.Monad.ST
+import qualified Data.Array.ST as ST
+import qualified Data.Array.MArray as M
+import qualified Data.Array.Unboxed as U
+import qualified Data.Array.IArray as I
+
+part1 :: Int -> [Int]
+part1 n = takeWhile (>=0) $ map fromIntegral $ I.elems a
+  where
+    a :: U.UArray Word32 Int8
+    a = ST.runSTUArray $
+          do arr <- ST.newArray (0,20) (-1)
+             size <- begin arr
+             _ <- tick arr size n
+             return arr
+
+begin arr = do
+  M.writeArray arr 0 3
+  M.writeArray arr 1 7
+  return 2
+
+tick a size n = go a n size 0 1
+  where
+    go _ 0 size _  _  = return size
+    go a n size e1 e2 = do
+      d1 <- M.readArray a e1
+      d2 <- M.readArray a e2
+      let (x,y) = (d1+d2) `divMod` 10
+      let size' | x > 0     = size + 2
+                | otherwise = size + 1
+      let e1' = (e1 + fromIntegral d1 + 1) `mod` size'
+          e2' = (e2 + fromIntegral d2 + 1) `mod` size'
+      dump <- takeWhile (>=0) <$> M.getElems a
+      if x > 0
+      then do M.writeArray a size x
+              M.writeArray a (size+1) y
+              traceShow dump $ go a (pred n) size' e1' e2'
+      else do M.writeArray a size y
+              traceShow dump $ go a (pred n) size' e1' e2'
+
+input = 846601
+
+main = do
+  print $ part1 16 -- print the example
