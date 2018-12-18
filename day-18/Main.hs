184f339a10dd33d6b0dc58f0e99b3c41539f6a31
Paolo <mrtnpaolo@gmail.com>
2018-12-18 06:46:51 +0100

    🛠 day-18 tick: double buffered

--- day-18/Main.hs
+++ day-18/Main.hs
@@ -1,3 +1,6 @@
+import Debug.Trace
+
+import Data.Foldable
 import Data.Bifunctor
 
 import Control.Arrow hiding ( second )
@@ -11,6 +14,7 @@ import qualified Data.Array.IArray  as A
 type Coord = (Int,Int)
 
 type STUA s = (Coord,Coord) -> [Int] -> ST s (A.STUArray s Coord Int)
+type STUA' s = (Coord,Coord) -> Int -> ST s (A.STUArray s Coord Int)
 
 data Acre = Open | Tree | Yard
   deriving (Show, Eq, Ord, Enum)
@@ -18,8 +22,20 @@ data Acre = Open | Tree | Yard
 -- | evolving a (0,0) (yM,xM) grid
 evolve :: Coord -> [(Coord,Acre)] -> Int -> A.UArray Coord Int
 evolve (h,w) yxs n = A.runSTUArray $ do
-  a <- (A.newListArray :: STUA s) ((0,0),(h-1,w-1)) . map (fromEnum . snd) $ yxs
-  return a
+  a <- (A.newListArray :: STUA s ) ((0,0),(h-1,w-1)) . map (fromEnum . snd) $ yxs
+  b <- (A.newArray     :: STUA' s) ((0,0),(h-1,w-1)) (-1)
+  forM_ [0..n] $ \i -> do
+    tick a b (i `mod` 2 == 0) -- First time a=a, b=b; then a=b, b=a; etc.
+  return b
+
+tick a0 a1 which = do
+  x <- A.readArray a (0,0)
+  traceShowM x
+  A.writeArray b (0,0) x
+  where
+    -- swap the buffers at each iteration
+    a | which = a0 | otherwise = a1
+    b | which = a1 | otherwise = a0
 
 parse = (largest &&& flatten) . map (second $ zip [0..] . map fromChar) . zip [0..] . lines
   where
@@ -34,4 +50,4 @@ main = do
   (size,coords) <- parse <$> readFile "test.txt"
   print coords
   print size
-  print $ evolve size coords 0
+  print $ evolve size coords 1
