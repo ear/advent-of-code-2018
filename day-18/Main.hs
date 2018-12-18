b427d69675ce416abdc938c02f28c962968e98ba
Paolo <mrtnpaolo@gmail.com>
2018-12-18 08:26:52 +0100

    🎄 day-18 part 2 in Haskell (side-calculation)
    Repeating pattern 498-526-554-...
    
    ```
    (498,183040)
    (499,184000)
    (500,184896)
    (501,184320)
    (502,182909)
    (503,183744)
    (504,183168)
    (505,182275)
    (506,183360)
    (507,183540)
    (508,182649)
    (509,180942)
    (510,182328)
    (511,182080)
    (512,183612)
    (513,185725)
    (514,187525)
    (515,189912)
    (516,194449)
    (517,195160)
    (518,198930)
    (519,197185)
    (520,193672)
    (521,191196)
    (522,187912)
    (523,185556)
    (524,183141)
    (525,183787)
    (526,183040)
    (527,184000)
    (528,184896)
    (529,184320)
    (530,182909)
    (531,183744)
    (532,183168)
    (533,182275)
    (534,183360)
    (535,183540)
    (536,182649)
    (537,180942)
    (538,182328)
    (539,182080)
    (540,183612)
    (541,185725)
    (542,187525)
    (543,189912)
    (544,194449)
    (545,195160)
    (546,198930)
    (547,197185)
    (548,193672)
    (549,191196)
    (550,187912)
    (551,185556)
    (552,183141)
    (553,183787)
    (554,183040)
    ```
    
    Calculation:
    
    ```
    $ gp --quiet
    ? 526 - 498
    28
    ? (554 - 498) - 28 * floor((554 - 498) / 28.0)
    0
    ? (1000000000 - 498) - 28 * floor((1000000000 - 498) / 28.0)
    26
    ? 27 = last, 26 = next to last = 183787
    ```

--- day-18/Main.hs
+++ day-18/Main.hs
@@ -2,7 +2,6 @@
 {-# language FlexibleContexts #-}
 import Debug.Trace
 
-import Data.STRef
 import Data.Foldable
 import Data.Bifunctor
 import Data.Traversable
@@ -16,8 +15,6 @@ import qualified Data.Array.Unboxed as A
 import qualified Data.Array.MArray  as A
 import qualified Data.Array.IArray  as A
 
-import qualified Data.HashMap.Strict as H
-
 type Coord = (Int,Int)
 
 type STUA s = (Coord,Coord) -> [Int] -> ST s (A.STUArray s Coord Int)
@@ -30,19 +27,18 @@ pattern OpenA = 0
 pattern TreeA = 1
 pattern YardA = 2
 
+resources area = count TreeA area * count YardA area
+
 -- | evolving a (0,0) (yM,xM) grid
 evolve :: Coord -> [(Coord,Acre)] -> Int -> A.UArray Coord Int
 evolve (h,w) yxs n = A.runSTUArray $ do
   a <- (A.newListArray :: STUA s ) ((0,0),(h-1,w-1)) . map (fromEnum . snd) $ yxs
   b <- (A.newArray     :: STUA' s) ((0,0),(h-1,w-1)) (-1)
-  a' <- A.getElems a
-  hs_ <- newSTRef $ H.singleton a' 0
   forM_ [1..n] $ \i -> do
-    when (i `mod` 10000 == 0) $ do
-      traceShowM i
-    b' <- tick (h,w) a b (i `mod` 2 == 0) -- First time a=a, b=b; then a=b, b=a; etc.
-    hs <- readSTRef hs_
-    writeSTRef hs_ $ H.alter (check i) b' hs
+    --when (i `mod` 10000 == 0) $ do
+    area <- A.getElems (if i `mod` 2 == 0 then b else a)
+    traceShowM (i,resources area)
+    tick (h,w) a b (i `mod` 2 == 0) -- First time a=a, b=b; then a=b, b=a; etc.
   return $ if (n `mod` 2 == 0) then a else b
 
 check :: Int -> Maybe Int -> Maybe Int
@@ -50,14 +46,13 @@ check _ Nothing = Just 1
 check j (Just i) = trace ("iteration " ++ show j ++ " repeats iteration " ++ show i) $ Just i
 
 tick (h,w) a0 a1 which = do
-  clear (h,w) b -- XXX not needed?
+  -- clear (h,w) b -- XXX not needed?
   forM_ [0..h-1] $ \y -> do
     forM_ [0..w-1] $ \x -> do
       n <- A.readArray a (y,x)
       ns <- neighbours h w a y x
       --traceShowM ns
       A.writeArray b (y,x) (next n ns)
-  A.getElems b
   where
     -- swap the buffers at each iteration
     a | which = a1 | otherwise = a0
@@ -99,7 +94,7 @@ parse = (largest &&& flatten) . map (second $ zip [0..] . map fromChar) . zip [0
 
 main :: IO ()
 main = do
-  (size,coords) <- parse <$> readFile "test.txt"
+  (size,coords) <- parse <$> readFile "input.txt"
   print size
   let area = evolve size coords 1000000000
   --print area
