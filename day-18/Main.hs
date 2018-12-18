5fc5b9937c4f188ba8aebcf4318d17be23acba19
Paolo <mrtnpaolo@gmail.com>
2018-12-18 06:35:31 +0100

    🛠 day-18 build initial array

--- day-18/Main.hs
+++ day-18/Main.hs
@@ -10,15 +10,15 @@ import qualified Data.Array.IArray  as A
 
 type Coord = (Int,Int)
 
-type STUA s = (Coord,Coord) -> Int -> ST s (A.STUArray s Coord Int)
+type STUA s = (Coord,Coord) -> [Int] -> ST s (A.STUArray s Coord Int)
 
 data Acre = Open | Tree | Yard
   deriving (Show, Eq, Ord, Enum)
 
 -- | evolving a (0,0) (yM,xM) grid
 evolve :: Coord -> [(Coord,Acre)] -> Int -> A.UArray Coord Int
-evolve (yM,xM) yxs n = A.runSTUArray $ do
-  a <- (A.newArray :: STUA s) ((0,0),(yM,xM)) (-1)
+evolve (h,w) yxs n = A.runSTUArray $ do
+  a <- (A.newListArray :: STUA s) ((0,0),(h-1,w-1)) . map (fromEnum . snd) $ yxs
   return a
 
 parse = (largest &&& flatten) . map (second $ zip [0..] . map fromChar) . zip [0..] . lines
